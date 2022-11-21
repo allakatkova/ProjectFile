eba311d74b2aeec4c0adfab49c9cd1d9e0a3046c
Alla Katkova <allakatkova@bk.ru>
2022-11-21 22:55:41 +0500

    task 1 complite

---
+++ main.py
@@ -0,0 +1,20 @@
+def read_content_file():
+    content = {}
+    with open('recipes.txt') as file:
+        for line in file:
+            recipes_name = line.strip()
+            recipe = {recipes_name: []}
+            ingredients_count = file.readline()
+            for i in range(int(ingredients_count)):
+                compound = file.readline()
+                ingredient_name, quantity, measure = compound.split(' | ')
+                products = {'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure.strip()}
+                recipe[recipes_name].append(products)
+            file.readline()
+            content.update(recipe)
+    return content
+
+
+if __name__ == '__main__':
+    cook_book = read_content_file()
+    print(cook_book)
