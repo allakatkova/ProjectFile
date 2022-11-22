ae46db159f8c2bdc702bc8a276b24e8028ec441f
Alla Katkova <allakatkova@bk.ru>
2022-11-22 23:30:43 +0500

    tasks 3 and 4 complite

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+
+
 def read_content_file():
     content = {}
     with open('recipes.txt') as file:
@@ -20,14 +23,10 @@ def get_shop_list_by_dishes(dishes, person_count):
     cook_book = read_content_file()
     for dish in dishes:
         ingredients_dish = cook_book.get(dish)
-        #print(ingredients_dish)
         for ingredient in ingredients_dish:
-            #print(ingredient)
             ingredient_name = ingredient.pop('ingredient_name')
-            #print(ingredient_name)
             required_quantity = ingredient.pop('quantity') * person_count
             ingredient.update({'quantity': required_quantity})
-            #print(ingredient)
             result_list.update({ingredient_name: ingredient})
     return result_list
 
@@ -44,7 +43,6 @@ if __name__ == '__main__':
 
     # task 2
     ingredients = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
-    # ingredients = get_shop_list_by_dishes(['Омлет'], 2)
     for key, value in ingredients.items():
         print(key, value)
 
@@ -52,3 +50,29 @@ if __name__ == '__main__':
     print('---------------------------------------')
     print()
 
+    # task 3
+    current_directory = os.getcwd()
+    folder = 'sorted'
+    files = ['1.txt', '2.txt', '3.txt']
+    result_file = 'result.txt'
+    list_content = []
+    line_count = 0
+    for file_txt in files:
+        full_path = os.path.join(current_directory, folder, file_txt)
+        with open(full_path, 'r') as file:
+            content = file.readlines()
+            content.insert(0, file_txt + '\n' + str(len(content)) + '\n')
+            if line_count > len(content):
+                list_content.insert(0, content)
+            else:
+                list_content.insert(len(list_content), content)
+            line_count = len(content)
+
+    full_path = os.path.join(current_directory, folder, result_file)
+
+    with open(full_path, 'w') as file:
+        for element in list_content:
+            for line in element:
+                file.writelines(line)
+
+    print(f'Результат слияния файлов записан в файл {full_path}')
